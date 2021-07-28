03117d76d49d1375c83f8b1af98567719159422c
Murotbek <74286358+Murotbek@users.noreply.github.com>
2021-07-28 20:16:08 +0500

    Add files via upload

--- js/jquerycods.js
+++ js/jquerycods.js
@@ -5,3 +5,15 @@
   });
   });
 })(jQuery);
+var $layer = $(".jquery-scroll");
+$layer.bind('touchstart', function (ev) {
+    var $this = $(this);
+    var layer = $layer.get(0);
+
+    if ($this.scrollTop() === 0) $this.scrollTop(1);
+    var scrollTop = layer.scrollTop;
+    var scrollHeight = layer.scrollHeight;
+    var offsetHeight = layer.offsetHeight;
+    var contentHeight = scrollHeight - offsetHeight;
+    if (contentHeight == scrollTop) $this.scrollTop(scrollTop-1);
+});
